4bf9b8cede8402c32c80fba09a3ccb9912b9e234
DanilaKotin011 <kotin.02@mail.ru>
2021-03-05 10:43:12 +0700

    update v2 chess.cpp code

--- chess.cpp
+++ chess.cpp
@@ -2,35 +2,34 @@
 #include <math.h>
 #include <stdio.h>
 
-
-char board[9][9]
-={
-    "rnbqkbnr",
-    "pppppppp",
-    "        ",
-    "        ",
-    "        ",
-    "        ",
-    "PPPPPPPP",
-    "RNBQKBNR"
-};
-
-void printboard()
-{
-    int i,z;
-    for(i=0;i<8;i++){
-        printf("%d| ",i+1);
-        for(z=0;z<8;z++)
-        {
-            printf("%c ",board[i][z]);
-        }
-        printf("\n");
-    }
-    printf("  -----------------  \n   a b c d e f g h");
-}
+void printboard(char[9][9]);
 
 int main()
 {
-    printboard();
+    char chessboard[9][9]
+        = {
+            "rnbqkbnr",
+            "pppppppp",
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "PPPPPPPP",
+            "RNBQKBNR"
+          };
+
+    printboard(chessboard);
     return 0;
+}
+
+void printboard(char board[9][9])
+{
+    for (int i = 0; i < 8; i++) {
+        printf("%d  ", i);
+        for (int z = 0; z < 8; z++) {
+            printf("%c", board[i][z]);
+        }
+        printf("\n");
+    }
+    printf(" \n  ---------   \n   abcdefgh");
 }
