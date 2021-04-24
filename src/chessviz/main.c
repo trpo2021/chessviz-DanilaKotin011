49e56c4c40c4298dbd1d283c42ca9015eb7cc418
DanilaKotin011 <kotin.02@mail.ru>
2021-04-24 10:49:50 +0700

    update main.c

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -1,20 +1,19 @@
 #include <libchessviz/move.h>
 #include <libchessviz/print.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    char chessboard[11][11] = {
-        "1rnbqkbnr",
-        "2pppppppp",
-        "3        ",
-        "4        ",
-        "5        ",
-        "6        ",
-        "7PPPPPPPP",
-        "8RNBQKBNR"
-    };
+    char chessboard[11][11]
+            = {"1rnbqkbnr",
+               "2pppppppp",
+               "3        ",
+               "4        ",
+               "5        ",
+               "6        ",
+               "7PPPPPPPP",
+               "8RNBQKBNR"};
     print(chessboard);
     while (1) {
         move(chessboard);
